bd0e1b1d22530e2ead0922c3f65e57db1456cadd
Nadin Yamaui <nadin@hqrentalsoftware.com>
2026-02-10 12:27:40 +0100

    RFC 015: configure Instagram Socialite provider

--- config/services.php
+++ config/services.php
@@ -35,4 +35,10 @@ return [
         ],
     ],
 
+    'instagram' => [
+        'client_id' => env('INSTAGRAM_CLIENT_ID'),
+        'client_secret' => env('INSTAGRAM_CLIENT_SECRET'),
+        'redirect' => env('INSTAGRAM_REDIRECT_URI'),
+    ],
+
 ];
