60bdf0e3a1c58d7311e928dee92cdbde6e96bf4f
Nadin Yamaui <nadin@hqrentalsoftware.com>
2026-02-11 16:16:41 +0100

    Clean

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,10 +5,8 @@ namespace App\Providers;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
-use SocialiteProviders\Manager\SocialiteWasCalled;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -47,11 +45,6 @@ class AppServiceProvider extends ServiceProvider
                 ->symbols()
                 ->uncompromised()
             : null
-        );
-
-        Event::listen(
-            SocialiteWasCalled::class,
-            \SocialiteProviders\Instagram\InstagramExtendSocialite::class.'@handle',
         );
     }
 }
