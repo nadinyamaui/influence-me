fd485046aaf7c8a5af299a6ac521a77e74677bd6
Nadin Yamaui <nadin@hqrentalsoftware.com>
2026-02-14 17:38:59 +0100

    Fix with stories retrieval

--- app/Services/Facebook/InstagramGraphService.php
+++ app/Services/Facebook/InstagramGraphService.php
@@ -80,10 +80,8 @@ class InstagramGraphService
             ], [
                 'instagram_account_id' => $this->account->id,
                 'media_type' => MediaType::Story,
-                'caption' => $story['caption'] ?? null,
-                'permalink' => $story['permalink'] ?? null,
-                'media_url' => $story['media_url'] ?? null,
-                'thumbnail_url' => $story['thumbnail_url'] ?? null,
+                'permalink' => $story['permalink'],
+                'media_url' => $story['media_url'],
                 'published_at' => Carbon::parse($story['timestamp']),
             ]);
         }
